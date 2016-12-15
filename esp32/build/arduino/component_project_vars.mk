e47b777af6c205a11840f83d1513d14c582c5dc0
murat ambarkutuk <m.ambarkutuk@gmail.com>
2016-12-15 16:43:46 -0500

    Delete the folder containing esp32-thing code and add it as a  submodule

--- esp32/build/arduino/component_project_vars.mk
+++
@@ -1,5 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/arduino/cores/esp32 $(PROJECT_PATH)/components/arduino/variants/esp32 $(PROJECT_PATH)/components/arduino/libraries/WiFi/src $(PROJECT_PATH)/components/arduino/libraries/SPI/src $(PROJECT_PATH)/components/arduino/libraries/Wire/src
-COMPONENT_LDFLAGS += -larduino
-COMPONENT_SUBMODULES += 
-arduino-build: 
